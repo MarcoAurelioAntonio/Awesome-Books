8c09d14260bbfb7a7395e4a3489b48b68fae1d06
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-08-08 17:11:01 -0300

    seting js

---
+++ index.js
@@ -0,0 +1,11 @@
+const titleSelector = document.querySelector('.title');
+const authorSelector = document.querySelector('.author');
+const addSelector = document.querySelector('.btn');
+
+const bookArr = [ 
+  {
+  title: 'dialogues',
+  author: 'plato',
+  }
+];
+
