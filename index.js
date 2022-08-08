03a1dab56eb247026b1b4e3cf4f443b65652a3bd
inakiesk <inakieskruiz@gmail.com>
2022-08-08 17:46:38 -0500

    Fix basic structure

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const listSelector = document.querySelector('.list');
 
 const bookArr = [ 
   {
-  title: 'Dialogues',
-  author: 'Plato',
+    title: 'Dialogues',
+    author: 'Plato',
   }
 ];
 
 const titleArr = [];
 const authorArr = [];
+const buttonArr = [];
 
 
 for (let i = 0; i < bookArr.length; i += 1) {
@@ -24,4 +25,24 @@ for (let i = 0; i < bookArr.length; i += 1) {
   titleArr[i].innerHTML = bookArr[i].title;
   authorArr[i].innerHTML = bookArr[i].author; 
 
+  buttonArr[i] = document.createElement('button');
+  buttonArr[i].innerHTML = 'Remove';
+  listSelector.appendChild(buttonArr[i]);
 }
+
+function addBook() {
+  let newBook = {   
+      title: '',
+      author: '',
+  }
+
+  titleSelector.value = newBook.title;
+
+  console.log(newBook);
+  console.log(newBook.title);
+  console.log(titleSelector.value);
+
+}
+
+addSelector.addEventListener('click', addBook());
+
