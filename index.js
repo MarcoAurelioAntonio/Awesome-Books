3101d31ceee543ff96d17f68ae0300f529948c5a
inakiesk <inakieskruiz@gmail.com>
2022-08-08 15:43:42 -0500

    Add book section dynamically created

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const titleSelector = document.querySelector('.title');
 const authorSelector = document.querySelector('.author');
 const addSelector = document.querySelector('.btn');
+const listSelector = document.querySelector('.list');
 
 const bookArr = [ 
   {
-  title: 'dialogues',
-  author: 'plato',
+  title: 'Dialogues',
+  author: 'Plato',
   }
 ];
 
-bookArr.forEach((item), ()=> {
+const titleArr = [];
+const authorArr = [];
 
-})
+
+for (let i = 0; i < bookArr.length; i += 1) {
+  titleArr[i] = document.createElement('h2');
+  authorArr[i] = document.createElement('p');
+
+  listSelector.appendChild(titleArr[i]);
+  listSelector.appendChild(authorArr[i]);
+
+  titleArr[i].innerHTML = bookArr[i].title;
+  authorArr[i].innerHTML = bookArr[i].author; 
+
+}
