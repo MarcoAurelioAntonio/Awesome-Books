eb4f861688a2f5d96a28a4f09fa6a3fffe2e1b9b
inakiesk <inakieskruiz@gmail.com>
2022-08-09 16:01:37 -0500

    Add remove function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const authorSelector = document.querySelector('.author');
 const addSelector = document.querySelector('.btn');
 const listSelector = document.querySelector('.list');
 
-const bookArr = [
+let bookArr = [
   {
     title: 'Dialogues',
     author: 'Plato',
@@ -37,10 +37,6 @@ for (let i = 0; i < bookArr.length; i += 1) {
   bookElement[i].appendChild(buttonArr[i]);
   console.log(buttonArr);
 
-  buttonArr[i].addEventListener('click', () => {
-    bookElement[i].remove();
-  });
-
 }
 
 addSelector.addEventListener('click', () => {
@@ -71,29 +67,35 @@ addSelector.addEventListener('click', () => {
     bookElement[i].appendChild(buttonArr[i]);
 
     buttonArr[i].addEventListener('click', () => {
-      bookElement[i].remove();
+
+      bookArr.splice(i, 1);
+
+      listSelector.textContent = '';
+      console.log(bookArr);
+      for (let i = 0; i < bookArr.length; i += 1) {
+
+        bookElement[i] = document.createElement('div');
+        listSelector.appendChild(bookElement[i]);
+    
+        titleArr[i] = document.createElement('h2');
+        authorArr[i] = document.createElement('p');
+    
+        bookElement[i].appendChild(titleArr[i]);
+        bookElement[i].appendChild(authorArr[i]);
+    
+        titleArr[i].innerHTML = bookArr[i].title;
+        authorArr[i].innerHTML = bookArr[i].author; 
+    
+        buttonArr[i] = document.createElement('button');
+        buttonArr[i].classList.add('remove-button');
+        buttonArr[i].innerHTML = 'Remove';
+        bookElement[i].appendChild(buttonArr[i]);
+      };
     });
-  }
+  };
   titleSelector.value = '';
   authorSelector.value = '';
 
 });
 
 
-
-/*buttonArr[].addEventListener('click', () => {
-  console.log('it works');
-});*/
-
-/*function showBook(book) {
-  const title = document.createElement('h2');
-  const author = document.createElement('p');
-  title.innerHTML = book.title;
-  author.innerHTML = book.author;
-  listSelector.appendChild(title);
-  listSelector.appendChild(author);
-  const button = document.createElement('button');
-  button.innerHTML = 'Remove';
-  listSelector.appendChild(button);
-  
-}*/
