2a9cd61cb2aec640d5053fc049fb232d0a9f8212
inakiesk <inakieskruiz@gmail.com>
2022-08-09 09:50:17 -0500

    Add the add books function

--- index.js
+++ index.js
@@ -2,20 +2,15 @@ const titleSelector = document.querySelector('.title');
 const authorSelector = document.querySelector('.author');
 const addSelector = document.querySelector('.btn');
 const listSelector = document.querySelector('.list');
-
-const bookArr = [ 
-  {
-    title: 'Dialogues',
-    author: 'Plato',
-  }
-];
-
+console.log(titleSelector);
+const bookArr = [];
+/*
 const titleArr = [];
 const authorArr = [];
-const buttonArr = [];
+const buttonArr = [];*/
 
 
-for (let i = 0; i < bookArr.length; i += 1) {
+/*for (let i = 0; i < bookArr.length; i += 1) {
   titleArr[i] = document.createElement('h2');
   authorArr[i] = document.createElement('p');
 
@@ -28,21 +23,38 @@ for (let i = 0; i < bookArr.length; i += 1) {
   buttonArr[i] = document.createElement('button');
   buttonArr[i].innerHTML = 'Remove';
   listSelector.appendChild(buttonArr[i]);
+}*/
+function showBook(book) {
+  const title = document.createElement('h2');
+  const author = document.createElement('p');
+  title.innerHTML = book.title;
+  author.innerHTML = book.author;
+  listSelector.appendChild(title);
+  listSelector.appendChild(author);
+  const button = document.createElement('button');
+  button.innerHTML = 'Remove';
+ /* button.addEventListener('click', removeBook(e))*/
+  listSelector.appendChild(button);
+
+
+  
 }
+addSelector.addEventListener('click', () => {
+    let newBook = {};
 
-function addBook() {
-  let newBook = {   
-      title: '',
-      author: '',
-  }
-
-  titleSelector.value = newBook.title;
-
-  console.log(newBook);
+  newBook.title = titleSelector.value;
+  newBook.author = authorSelector.value;
+  bookArr.push(newBook);
+  showBook(newBook);
+  console.log(bookArr);
+  titleSelector.value = '';
+  authorSelector.value = '';
   console.log(newBook.title);
-  console.log(titleSelector.value);
+  console.log(titleSelector);
+  console.log(newBook.author);
+  console.log(authorSelector);
+  console.log(newBook);
 
-}
+});
 
-addSelector.addEventListener('click', addBook());
 
