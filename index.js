96193432921415e6b064201bc1f73397e15f266b
inakiesk <inakieskruiz@gmail.com>
2022-08-09 11:22:33 -0500

    Add some structure for add button

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const titleSelector = document.querySelector('.title');
 const authorSelector = document.querySelector('.author');
 const addSelector = document.querySelector('.btn');
 const listSelector = document.querySelector('.list');
-console.log(titleSelector);
+
 const bookArr = [];
-/*
+
 const titleArr = [];
 const authorArr = [];
-const buttonArr = [];*/
+const buttonArr = [];
 
 
-/*for (let i = 0; i < bookArr.length; i += 1) {
+for (let i = 0; i < bookArr.length; i += 1) {
   titleArr[i] = document.createElement('h2');
   authorArr[i] = document.createElement('p');
 
@@ -23,8 +23,8 @@ const buttonArr = [];*/
   buttonArr[i] = document.createElement('button');
   buttonArr[i].innerHTML = 'Remove';
   listSelector.appendChild(buttonArr[i]);
-}*/
-function showBook(book) {
+}
+/*function showBook(book) {
   const title = document.createElement('h2');
   const author = document.createElement('p');
   title.innerHTML = book.title;
@@ -33,20 +33,36 @@ function showBook(book) {
   listSelector.appendChild(author);
   const button = document.createElement('button');
   button.innerHTML = 'Remove';
- /* button.addEventListener('click', removeBook(e))*/
   listSelector.appendChild(button);
-
-
   
-}
+}*/
 addSelector.addEventListener('click', () => {
     let newBook = {};
 
   newBook.title = titleSelector.value;
   newBook.author = authorSelector.value;
+  listSelector.remove();
+  const bookWrapper = document.createElement('div');
   bookArr.push(newBook);
-  showBook(newBook);
   console.log(bookArr);
+  for (let i = 0; i < bookArr.length; i += 1) {
+    const listSelector = createElement('div');
+    listSelector.className = 'list';
+    
+
+    titleArr[i] = document.createElement('h2');
+    authorArr[i] = document.createElement('p');
+
+    listSelector.appendChild(titleArr[i]);
+    listSelector.appendChild(authorArr[i]);
+
+    titleArr[i].innerHTML = bookArr[i].title;
+    authorArr[i].innerHTML = bookArr[i].author; 
+
+    buttonArr[i] = document.createElement('button');
+    buttonArr[i].innerHTML = 'Remove';
+    listSelector.appendChild(buttonArr[i]);
+  }
   titleSelector.value = '';
   authorSelector.value = '';
   console.log(newBook.title);
