c917c75d3d3ca12c37c97ede73439f59ece1e7c1
inakiesk <inakieskruiz@gmail.com>
2022-08-09 12:39:45 -0500

    Fix add book function

--- index.js
+++ index.js
@@ -1,67 +1,68 @@
+const bookContainer = document.querySelector('.container');
 const titleSelector = document.querySelector('.title');
 const authorSelector = document.querySelector('.author');
 const addSelector = document.querySelector('.btn');
 const listSelector = document.querySelector('.list');
 
-const bookArr = [];
+const bookArr = [
+  {
+    title: 'Dialogues',
+    author: 'Plato',
+  }
+];
 
 const titleArr = [];
 const authorArr = [];
 const buttonArr = [];
+const bookElement = [];
 
 
 for (let i = 0; i < bookArr.length; i += 1) {
+
+  bookElement[i] = document.createElement('div');
+  listSelector.appendChild(bookElement[i]);
+
   titleArr[i] = document.createElement('h2');
   authorArr[i] = document.createElement('p');
 
-  listSelector.appendChild(titleArr[i]);
-  listSelector.appendChild(authorArr[i]);
+  bookElement[i].appendChild(titleArr[i]);
+  bookElement[i].appendChild(authorArr[i]);
 
   titleArr[i].innerHTML = bookArr[i].title;
   authorArr[i].innerHTML = bookArr[i].author; 
 
   buttonArr[i] = document.createElement('button');
   buttonArr[i].innerHTML = 'Remove';
-  listSelector.appendChild(buttonArr[i]);
+  bookElement[i].appendChild(buttonArr[i]);
+
 }
-/*function showBook(book) {
-  const title = document.createElement('h2');
-  const author = document.createElement('p');
-  title.innerHTML = book.title;
-  author.innerHTML = book.author;
-  listSelector.appendChild(title);
-  listSelector.appendChild(author);
-  const button = document.createElement('button');
-  button.innerHTML = 'Remove';
-  listSelector.appendChild(button);
-  
-}*/
+
 addSelector.addEventListener('click', () => {
     let newBook = {};
 
   newBook.title = titleSelector.value;
   newBook.author = authorSelector.value;
-  listSelector.remove();
-  const bookWrapper = document.createElement('div');
+  listSelector.textContent = '';
+  
   bookArr.push(newBook);
   console.log(bookArr);
   for (let i = 0; i < bookArr.length; i += 1) {
-    const listSelector = createElement('div');
-    listSelector.className = 'list';
-    
+
+    bookElement[i] = document.createElement('div');
+    listSelector.appendChild(bookElement[i]);
 
     titleArr[i] = document.createElement('h2');
     authorArr[i] = document.createElement('p');
 
-    listSelector.appendChild(titleArr[i]);
-    listSelector.appendChild(authorArr[i]);
+    bookElement[i].appendChild(titleArr[i]);
+    bookElement[i].appendChild(authorArr[i]);
 
     titleArr[i].innerHTML = bookArr[i].title;
     authorArr[i].innerHTML = bookArr[i].author; 
 
     buttonArr[i] = document.createElement('button');
     buttonArr[i].innerHTML = 'Remove';
-    listSelector.appendChild(buttonArr[i]);
+    bookElement[i].appendChild(buttonArr[i]);
   }
   titleSelector.value = '';
   authorSelector.value = '';
@@ -74,3 +75,15 @@ addSelector.addEventListener('click', () => {
 });
 
 
+/*function showBook(book) {
+  const title = document.createElement('h2');
+  const author = document.createElement('p');
+  title.innerHTML = book.title;
+  author.innerHTML = book.author;
+  listSelector.appendChild(title);
+  listSelector.appendChild(author);
+  const button = document.createElement('button');
+  button.innerHTML = 'Remove';
+  listSelector.appendChild(button);
+  
+}*/
