fddc2a2051ee2f235824487978db63f4db5ba594
inakiesk <inakieskruiz@gmail.com>
2022-08-10 16:17:20 -0500

    Link remove function with local storage

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ let bookArr = [
   }
 ];
 
-getInfo();
+
 
 const titleArr = [];
 const authorArr = [];
@@ -45,22 +45,16 @@ for (let i = 0; i < bookArr.length; i += 1) {
   buttonArr[i].classList.add('remove-button');
   buttonArr[i].innerHTML = 'Remove';
   bookElement[i].appendChild(buttonArr[i]);
-
 }
 
 addSelector.addEventListener('click', () => {
     let newBook = {};
-
   storeInfo();
   newBook.title = titleSelector.value;
   newBook.author = authorSelector.value;
   listSelector.textContent = '';
-  
   bookArr.push(newBook);
   localStorage.setItem('book', JSON.stringify(bookArr));
-  console.log(JSON.parse(localStorage.getItem('book')));
-  console.log(bookArr);
-  const savedData = JSON.parse(localStorage.getItem('book'));
   for (let i = 0; i < bookArr.length; i += 1) {
 
     bookElement[i] = document.createElement('div');
@@ -79,20 +73,31 @@ addSelector.addEventListener('click', () => {
     buttonArr[i].classList.add('remove-button');
     buttonArr[i].innerHTML = 'Remove';
     bookElement[i].appendChild(buttonArr[i]);
-
+    
   };
-  titleSelector.value = '';
-  authorSelector.value = '';
-
+  
 });
 
 // remove btn starts here
+function removeItem(item) {
+  bookArr = bookArr.filter((el) => el.title !== item.textContent);
+  localStorage.setItem('book', JSON.stringify(bookArr));
+  console.log(localStorage);
+  console.log(bookArr, 'remove');
+  console.log('remove book', localStorage.getItem('book'));
+
+}
+
 const x = document.getElementsByClassName('list')[0];
 x.addEventListener('click', (e) => {
   if (e.target.textContent === 'Remove'){
+    removeItem(e.target.parentElement.firstChild)
     e.target.parentElement.remove();
-  }
-});
+    }
+  });
+
+
+/*e.target.parentElement.remove();*/
 
   // ......................................................... //
 
@@ -106,18 +111,29 @@ function storeInfo() {
 function getInfo() {
   const obtainedInfo = JSON.parse(localStorage.getItem('book'));
   if (bookInfo.title !== '') {
-  bookArr.push(obtainedInfo);
+    bookArr.push(obtainedInfo);
+    console.log(obtainedInfo);
+    for (let i = 0; i < bookArr.length; i += 1) {
+      document.querySelector('.list').innerHTML = `
+        <div>
+          <h2>${bookArr[i].title}</h2>
+          <p>${bookArr[i].author}</p>
+          <button class="remove-button">Remove</button>
+        </div>
+      `;
+      console.log('hello');
+    }
   }
 }
+getInfo();
 
 function localCheck() {
-  if (localStorage.getItem('book') !== null) {
+  if (!localStorage.getItem('book')) {
     localStorage.setItem('book', JSON.stringify(bookInfo));
   }
 }
 
 function display(displayBook) {
-  console.log(displayBook);
   localStorage.setItem('book', JSON.stringify(bookArr));
 
     const newBookElement = document.createElement('div');
@@ -138,11 +154,11 @@ function display(displayBook) {
     newBookElement.appendChild(newButton);
 }
 
-/*bookArry[i].title = savedData[I].title*/
-/*for (let I = 0; I< savedData.length; I++)*/
 
 const savedData = JSON.parse(localStorage.getItem('book'));
+
+/*
 for (let i = 0; i < savedData.length; i+=1) {
   bookArr[i].title = savedData[i].title
   bookArr[i].author = savedData[i].author
-}
+};*/
